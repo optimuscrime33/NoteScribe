07ec97ebe4c2545f2f5f8c8709f113292c29bc82
Justin L. Patino <justinlpatino@gmail.com>
2019-09-30 20:30:43 -0500

    stash spinner stuff in favor of NumberPicker

--- app/src/main/java/com/justinpatino/notescribe/CreateNewProjectActivity.java
+++ app/src/main/java/com/justinpatino/notescribe/CreateNewProjectActivity.java
@@ -1,14 +1,15 @@
 package com.justinpatino.notescribe;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
 
-public class CreateNewProjectActivity extends AppCompatActivity {
+public class CreateNewProjectActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +24,34 @@ public class CreateNewProjectActivity extends AppCompatActivity {
 
         getWindow().setLayout((int) (0.8 * dmWidth),(int) (0.8 * dmHeight));
 
+        String[] numeratorArr = R.array.timeSigArray;
+
+
+        NumberPicker timeSigNumeratorNp = (NumberPicker) findViewById(R.id.timeSigNumerator);
+        timeSigNumeratorNp.setMaxValue(R.array.timeSigArray.length - 1);
+        timeSigNumeratorNp.setDisplayedValues(R.array.timeSigArray);
+
+        /*Spinner timeSigNumeratorSpinner = (Spinner) findViewById(R.id.timeSigNumerator);
+        Spinner timeSigDenominatorSpinner = (Spinner) findViewById(R.id.timeSigDenominator);
+       ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.timeSigArray, R.layout.time_signature_spinner_layout);
+        adapter.setDropDownViewResource(R.layout.time_signature_spinner_dropdown_layout);
+        timeSigNumeratorSpinner.setAdapter(adapter);
+        timeSigNumeratorSpinner.setOnItemSelectedListener(this);
+        timeSigDenominatorSpinner.setAdapter(adapter);
+        timeSigDenominatorSpinner.setOnItemSelectedListener(this);*/
+
+
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
