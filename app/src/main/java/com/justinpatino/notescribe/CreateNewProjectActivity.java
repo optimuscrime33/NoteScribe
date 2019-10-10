ac2a350d328b1f7d8b4d44dbfd4957ced64eb6d9
Justin L. Patino <justinlpatino@gmail.com>
2019-10-09 19:04:01 -0500

    Obtain number picker values

--- app/src/main/java/com/justinpatino/notescribe/CreateNewProjectActivity.java
+++ app/src/main/java/com/justinpatino/notescribe/CreateNewProjectActivity.java
@@ -7,9 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.NumberPicker;
 
 public class CreateNewProjectActivity extends AppCompatActivity {
-
+    public static int timeSignatureNumerator = 4;
+    public static int timeSignatureDenominator = 4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +26,45 @@ public class CreateNewProjectActivity extends AppCompatActivity {
 
         getWindow().setLayout((int) (0.8 * dmWidth),(int) (0.8 * dmHeight));
 
+        String[] timeSigArray = new String[16];
+        for(int i = 0; i < 16; i++) {
+            timeSigArray[i] = Integer.toString(i + 1);
+        }
+
+        NumberPicker timeSigNpNumerator = (NumberPicker) findViewById(R.id.timeSignatureNumerator);
+        timeSigNpNumerator.setMaxValue(timeSigArray.length - 1);
+        timeSigNpNumerator.setDisplayedValues(timeSigArray);
+        timeSigNpNumerator.setValue(timeSignatureNumerator - 1);
+
+        NumberPicker timeSigNpDenominator = (NumberPicker) findViewById(R.id.timeSignatureDenominator);
+        timeSigNpDenominator.setMaxValue(timeSigArray.length - 1);
+        timeSigNpDenominator.setDisplayedValues(timeSigArray);
+        timeSigNpDenominator.setValue(timeSignatureDenominator - 1);
+
+        timeSigNpNumerator.setOnValueChangedListener(onNumeratorValueChangeListener);
+        timeSigNpDenominator.setOnValueChangedListener(onDenominatorValueChangeListener);
+
+    }
+
+    NumberPicker.OnValueChangeListener onNumeratorValueChangeListener = new NumberPicker.OnValueChangeListener() {
+        @Override
+        public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+            timeSignatureNumerator = numberPicker.getValue();
+        }
+    };
+
+    NumberPicker.OnValueChangeListener onDenominatorValueChangeListener = new NumberPicker.OnValueChangeListener() {
+        @Override
+        public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+            timeSignatureDenominator = numberPicker.getValue();
+        }
+    };
+
+    public void initializeProject(View view) {
+      /*  Intent intent = new Intent(this, EditProjectActivity.class);*/
+        EditText projectName = (EditText) findViewById(R.id.projectNameEditText);
+        String message = projectName.getText().toString();
+        /*intent.putExtra();*/
     }
 
 
